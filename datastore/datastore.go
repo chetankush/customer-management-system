eaee0a3ac37b0d181528a321b19d5cd07b548ffa
chetankush <dineshkushwah5252@gmail.com>
2023-12-14 20:16:20 +0530

    updates in datastore and model

--- datastore/datastore.go
+++ datastore/datastore.go
@@ -1,9 +1,10 @@
-package datastore 
+package datastore
 
 import (
+	"car-service/model"
 	"database/sql"
 	"strconv"
-	"car-service/model"
+
 	"gofr.dev/pkg/errors"
 	"gofr.dev/pkg/gofr"
 )
@@ -17,9 +18,9 @@ func NewCustomerDatastore() *CustomerDatastore {
 func (d *CustomerDatastore) Create(ctx *gofr.Context, customer *model.Customer) (*model.Customer, error) {
 	var resp model.Customer
 
-	queryInsert := "INSERT INTO customers (id, name) VALUES (?, ?)"
+	queryInsert := "INSERT INTO customers (id, name, email, phone, address, city, date_of_birth, is_active) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
 
-	result, err := ctx.DB().ExecContext(ctx, queryInsert, customer.ID, customer.Name)
+	result, err := ctx.DB().ExecContext(ctx, queryInsert, customer.ID, customer.Name, customer.Email, customer.Phone, customer.Address, customer.City, customer.DateOfBirth, customer.IsActive)
 
 	if err != nil {
 		return &model.Customer{}, errors.DB{Err: err}
@@ -30,10 +31,10 @@ func (d *CustomerDatastore) Create(ctx *gofr.Context, customer *model.Customer) 
 		return &model.Customer{}, errors.DB{Err: err}
 	}
 
-	querySelect := "SELECT id, name FROM customers WHERE id = ?"
+	querySelect := "SELECT id, name, email, phone, address, city, date_of_birth, is_active FROM customers WHERE id = ?"
 
 	err = ctx.DB().QueryRowContext(ctx, querySelect, lastInsertID).
-		Scan(&resp.ID, &resp.Name)
+		Scan(&resp.ID, &resp.Name, &resp.Email, &resp.Phone, &resp.Address, &resp.City, &resp.DateOfBirth, &resp.IsActive)
 
 	if err != nil {
 		return &model.Customer{}, errors.DB{Err: err}
@@ -42,37 +43,34 @@ func (d *CustomerDatastore) Create(ctx *gofr.Context, customer *model.Customer) 
 	return &resp, nil
 }
 
-
-func (d *CustomerDatastore) GetAll(ctx *gofr.Context) ([]model.Customer,error){
+func (d *CustomerDatastore) GetAll(ctx *gofr.Context) ([]model.Customer, error) {
 	var customers []model.Customer
 
-	rows,err := ctx.DB().QueryContext(ctx, "SELECT id, name FROM customers")
+	rows, err := ctx.DB().QueryContext(ctx, "SELECT  id, name, email, phone, address, city, date_of_birth, is_active FROM customers")
 	if err != nil {
-		return nil, errors.DB{Err:err}
+		return nil, errors.DB{Err: err}
 	}
 
-	for rows.Next(){
+	for rows.Next() {
 		var customer model.Customer
-		if err := rows.Scan(&customer.ID, &customer.Name); err != nil{
-			return nil, errors.DB{Err:err}
+		if err := rows.Scan(&customer.ID, &customer.Name, &customer.Email, &customer.Phone, &customer.Address, &customer.City, &customer.DateOfBirth, &customer.IsActive); err != nil {
+			return nil, errors.DB{Err: err}
 		}
 		customers = append(customers, customer)
 	}
 	return customers, nil
 }
 
-
-
-func (d *CustomerDatastore) GetByID(ctx *gofr.Context, id string) (*model.Customer,error){
+func (d *CustomerDatastore) GetByID(ctx *gofr.Context, id string) (*model.Customer, error) {
 	var resp model.Customer
 
-	querySelect := "SELECT id, name FROM customers WHERE id = ?"
+	querySelect := "SELECT id, name, email, phone, address, city, date_of_birth, is_active FROM customers WHERE id = ?"
 
-	err := ctx.DB().QueryRowContext(ctx, querySelect, id).Scan(&resp.ID, &resp.Name)
+	err := ctx.DB().QueryRowContext(ctx, querySelect, id).Scan(&resp.ID, &resp.Name, &resp.Email, &resp.Phone, &resp.Address, &resp.City, &resp.DateOfBirth, &resp.IsActive)
 
 	switch err {
 	case sql.ErrNoRows:
-		return &model.Customer{},errors.EntityNotFound{Entity: "customer", ID:id}
+		return &model.Customer{}, errors.EntityNotFound{Entity: "customer", ID: id}
 	case nil:
 		return &resp, nil
 	default:
@@ -80,9 +78,8 @@ func (d *CustomerDatastore) GetByID(ctx *gofr.Context, id string) (*model.Custom
 	}
 }
 
-
-func (d *CustomerDatastore) Update(ctx *gofr.Context, customer *model.Customer) (*model.Customer, error){
-    _, err := ctx.DB().ExecContext(ctx, "UPDATE customers SET name = ? WHERE id = ?", customer.Name, customer.ID)
+func (d *CustomerDatastore) Update(ctx *gofr.Context, customer *model.Customer) (*model.Customer, error) {
+	_, err := ctx.DB().ExecContext(ctx, "UPDATE customers SET name = ?, email = ?, phone = ?, address = ?, city = ?, date_of_birth = ?, is_active = ? WHERE id = ?", customer.IsActive, customer.DateOfBirth, customer.City, customer.Address, customer.Phone, customer.Email, customer.Name, customer.ID)
 	if err != nil {
 		return nil, errors.DB{Err: err}
 	}
@@ -90,12 +87,11 @@ func (d *CustomerDatastore) Update(ctx *gofr.Context, customer *model.Customer) 
 	return d.GetByID(ctx, strconv.Itoa(customer.ID))
 }
 
-
 func (d *CustomerDatastore) Delete(ctx *gofr.Context, id string) error {
 	queryDelete := "DELETE FROM customers WHERE id = ?"
 
 	_, err := ctx.DB().ExecContext(ctx, queryDelete, id)
-	
+
 	if err != nil {
 		return errors.DB{Err: err}
 	}
--- model/model.go
+++ model/model.go
@@ -1,7 +1,12 @@
 package model
 
 type Customer struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Email       string `json:"email"`
+	Phone       string `json:"phone"`
+	Address     string `json:"address"`
+	City        string `json:"city"`
+	DateOfBirth string `json:"date_of_birth"`
+	IsActive    bool   `json:"is_active"`
 }
